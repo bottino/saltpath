40715c1a68949ebe8896416c8ea4f8411d3875ba
Baptiste Ottino <baptiste.ottino@gmail.com>
2025-07-01 14:00:09 +0200

    Command line interface and fix small bug

--- README.md
+++ README.md
@@ -1,3 +1,23 @@
 # saltpath
 
 Gets the course of a sailing ship based on a GPX file.
+
+## Setup
+
+Use [uv](https://docs.astral.sh/uv/)
+
+```bash
+uv sync
+```
+
+Then, to do a simple acquisition in the current directory, run:
+
+```bash
+uv run saltpath.py <your-gpx-file> .
+```
+
+You can find the different options like so:
+
+```bash
+uv run saltpath.py --help
+```
--- saltpath.py
+++ saltpath.py
@@ -1,36 +1,50 @@
-import gpxpy
+import argparse
+import math
+import os
+import shutil
 
 import geopandas as gpd
-import math
+import gpxpy
 import numpy as np
 import pandas as pd
-
 from shapely.geometry import LineString, Point
 
-TOLERANCE = 200.0
 
+def main(input_file, output_dir, tolerance, dir_name):
+    if dir_name is None:
+        name_gdf = gpd.read_file(input_file, layer="tracks")
+        dir_name = name_gdf.iloc[0]["name"]
 
-# Use RDP algorithm: https://en.wikipedia.org/wiki/Ramer%E2%80%93Douglas%E2%80%93Peucker_algorithm
-def main():
-    gdf = gpd.read_file("./raw_path.gpx", layer="track_points")
+    if os.path.exists(dir_name) and os.path.isdir(dir_name):
+        shutil.rmtree(dir_name)
+
+    os.makedirs(dir_name)
+    output_path = os.path.join(output_dir, dir_name)
+
+    # Read file
+    gdf = gpd.read_file(input_file, layer="track_points")
     gdf = gdf.dropna(axis=1, how="all")
 
     # convert to https://epsg.io/21781
     proj_gdf = gdf.to_crs(21781)
-    print(proj_gdf.head())
-    path_gdf = simplify_path(proj_gdf, TOLERANCE)
+
+    # simplify the track
+    path_gdf = simplify_path(proj_gdf, tolerance)
 
     # join with the timestamps and indices
     path_gdf = gpd.sjoin(path_gdf, proj_gdf, how="left")
 
     # export the simplified path to inspect as gpx
-    export_to_gpx(path_gdf, "./output/simplified_path.gpx")
+    export_to_gpx(path_gdf, os.path.join(output_path, "simplified_path.gpx"))
+    # copy the raw data file
+    shutil.copy(input_file, output_path)
 
     # output tacks
     tacks_df = get_tacks(path_gdf)
-    tacks_df.to_csv("./output/tacks.csv", index=None)
+    tacks_df.to_csv(os.path.join(output_path, "tacks.csv"), index=None)
 
 
+# Use RDP algorithm: https://en.wikipedia.org/wiki/Ramer%E2%80%93Douglas%E2%80%93Peucker_algorithm
 def simplify_path(gdf: gpd.GeoDataFrame, tolerance: float) -> gpd.GeoDataFrame:
     path = LineString(gdf.geometry.tolist())
     simple_line = path.simplify(tolerance, preserve_topology=False)
@@ -59,7 +73,7 @@ def get_tacks(gdf: gpd.GeoDataFrame) -> pd.DataFrame:
             "start_point": list(zip(round(gdf.x, 2), round(gdf.y, 2))),
             "end_point": list(zip(round(gdf.x_next, 2), round(gdf.y_next, 2))),
             "start_time": gdf.time,
-            "end_time": gdf.time.shift(-1),
+            "end_time": gdf.time_next,
             "distance_m": round(
                 np.sqrt(
                     (gdf["x_next"] - gdf["x"]) ** 2 + (gdf["y_next"] - gdf["y"]) ** 2
@@ -109,5 +123,34 @@ def export_to_gpx(gdf: gpd.GeoDataFrame, output_file: str) -> None:
         f.write(gpx.to_xml())
 
 
+# Step 3: Function to parse command-line arguments
+def parse_arguments():
+    parser = argparse.ArgumentParser(
+        description="Simplify a track on a GPX file, and outputs the simplified path and the tacks to an output directory."
+    )
+    parser.add_argument("input_file", type=str, help="Path to the input GPX file")
+    parser.add_argument(
+        "output_dir",
+        type=str,
+        help="Directory where the output is stored",
+    )
+    parser.add_argument(
+        "-t",
+        "--tolerance",
+        type=float,
+        default=200.0,
+        help="Tolerance value for simplification in meters. Higher values will result in a trajectory with fewer points.",
+    )
+    parser.add_argument(
+        "-d",
+        "--dir_name",
+        type=str,
+        default=None,
+        help="Name of the output directory for the acquisition",
+    )
+    return parser.parse_args()
+
+
 if __name__ == "__main__":
-    main()
+    args = parse_arguments()
+    main(args.input_file, args.output_dir, args.tolerance, args.dir_name)
